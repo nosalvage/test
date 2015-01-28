4455151d8a0100d9c91a57c8936bf0aba6c43ef8
Peter <genuris@genuris-Aspire-5750G>
2015-01-28 17:30:33 +0600

    First commit

---
+++ email.php
@@ -0,0 +1,22 @@
+
+<?php
+
+$name = $_POST["name"];
+$email = $_POST["email"];
+$phone = $_POST["phone"];
+$link = $_POST["link"];
+$pocket = $_POST["pocket"];
+$MailTo = "linc.order@gmail.com"; 
+
+
+	if(mail ($MailTo,
+      "Новый заказ (от: $name)",
+      " Имя: $name \n Ссылка vk: $link \n Телефон: $phone \n Email: $email",
+      "Content-type:text/plain; charset=utf-8")){
+		header("Location: ../home.html");
+	} else{
+		echo "Ошибка отправления заявки, свяжитесь с нами 87079525955";
+	}
+
+
+?>
